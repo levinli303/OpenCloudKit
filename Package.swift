2e81cdf16d700309abfccfaf5238716684e8e999
lilinfeng <lilinfeng303@outlook.com>
2019-10-30 19:25:03 +0800

    remove redundant files

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.1
+// swift-tools-version:5.1
 import PackageDescription
 
 #if os(Linux)
@@ -10,16 +10,13 @@ import PackageDescription
 let package = Package(
     name: "OpenCloudKit",
     products: [
-    	.library(name: "OpenCloudKit", targets: ["OpenCloudKit"]),
+        .library(name: "OpenCloudKit", targets: ["OpenCloudKit"]),
     ],
     dependencies: [
-        .package(url: cOpenSSLRepo, from: "4.0.0"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "0.11.0"),
+        .package(url: cOpenSSLRepo, from: "4.0.1"),
+        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.1.3"),
     ],
     targets: [
-    	.target(name: "OpenCloudKit", dependencies: [
-    		"COpenSSL",
-    		"CryptoSwift",
-    	])
+        .target(name: "OpenCloudKit", dependencies: ["COpenSSL", "CryptoSwift",])
     ]
 )
