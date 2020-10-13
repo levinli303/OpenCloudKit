0184e446b901a5d438b9cd9f1c7202dbc2ccf5fe
Levin Li <lilinfeng303@outlook.com>
2020-10-13 17:08:49 +0800

    Add test for fetching record with asset

--- Tests/OpenCloudKitTests/CKAssetTests.swift
+++ Tests/OpenCloudKitTests/CKAssetTests.swift
@@ -20,12 +20,37 @@ class CKAssetTests: XCTestCase {
         record["editor"] = "Developer" as CKRecordValue
         record["image"] = CKAsset(fileURL: URL(fileURLWithPath: "splash.png"))
 
-        let expectation = XCTestExpectation(description: "ensureExecuteOnMainThread")
+        let expectation = XCTestExpectation(description: "Wait for finish")
         db.save(record: record) { record, error in
             XCTAssertNil(error)
             XCTAssertNotNil(record)
             expectation.fulfill()
         }
         wait(for: [expectation], timeout: 10)
+    }
+
+    func testFetchRecordWithAsset() {
+        let recordID = "3A5DDE96-0C61-46DE-BB8F-EBC2D4B08A14"
+        let db = CKContainer.default().publicCloudDatabase
+        let query = CKQuery(
+            recordType: "Quanzi",
+            filters: [CKQueryFilter(fieldName: "id", comparator: .equals, fieldValue: recordID)]
+        )
+        let op = CKQueryOperation(query: query)
+        op.resultsLimit = 1
+        var records = [CKRecord]()
+        var error: Error?
+        let expectation = XCTestExpectation(description: "Wait for finish")
+        op.recordFetchedBlock = { record in
+            records.append(record)
+        }
+        op.queryCompletionBlock = { cursor, e in
+            error = e
+            expectation.fulfill()
+        }
+        db.add(op)
+        wait(for: [expectation], timeout: 10)
+        XCTAssertNil(error)
+        XCTAssertEqual(records.count, 1)
     }
 }
